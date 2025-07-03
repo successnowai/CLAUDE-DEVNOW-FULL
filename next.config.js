51deb4eadd429cf21ac658c44a7aaab18d85f3eb
successnowai <info@successnow.ai>
2025-07-03 11:24:40 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['images.unsplash.com', 'via.placeholder.com'],
+  },
+  env: {
+    KV_URL: process.env.KV_URL,
+    KV_REST_API_URL: process.env.KV_REST_API_URL,
+    KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN,
+    POSTGRES_URL: process.env.POSTGRES_URL,
+  },
+}
+
+module.exports = nextConfig
